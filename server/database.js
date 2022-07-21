4cc720c56282c8e802cc6529de3b450194fef4ac
Cisco Song <weijiesong@umass.edu>
2022-07-21 15:12:27 -0400

    change(database.js): add addTable dropTable getAvgGrade getAvgEvaluation and writeData

---
+++ server/database.js
@@ -0,0 +1,72 @@
+import pg from "pg";
+
+// Get the Pool class from the pg module.
+const { Pool } = pg;
+
+// A class representing a database to store scores.
+export class Database {
+   constructor(URL) {
+      this.dburl = URL;
+   }
+
+   async connect() {
+      this.pool = new Pool(
+         {
+            connectionString: this.dburl,
+            ssl: { rejectUnauthorized: false } // Required for Heroku connections
+         }
+      );
+
+      // Create the pool.
+      this.client = await this.pool.connect();
+   }
+
+   async addTable(name) {
+      const queryText = `
+         CREATE TABLE IF NOT EXISTS ${name} (
+            semester VARCHAR(30),
+            professor VARCHAR(30),
+            evaluation INTEGER,
+            grade NUMERIC(4, 3)
+         );
+      `;
+   }
+
+   async dropTable(name) {
+      const queryText = `DROP TABLE IF EXISTS ${name};`;
+   }
+
+   async getAvgGrade(course, semester) {
+      const queryText = `
+         SELECT AVG(grade) FROM (
+            SELECT * FROM ${course}
+            WHERE semester = ${semester};
+         );
+      `;
+      const res = await this.client.query(queryText);
+      return JSON.parse(res.rows);
+   }
+
+   async getAvgEvaluation(course, semester, professor) {
+      const queryText = `
+         SELECT AVG(evaluation) FROM (
+            SELECT * FROM ${course}
+            WHERE semester = ${semester}
+            AND professor = ${professor};
+         );
+      `;
+      const res = await this.client.query(queryText);
+      return JSON.parse(res.rows);
+   }
+
+   async writeData(course, semester, professor, evaluation, grade) {
+      await addTable(course);
+      const queryText = `
+         INSERT INTO ${course} (semester, professor, evaluation, grade)
+         VALUES ($1, $2, $3, $4)
+         RETURNING *;
+      `;
+      const res = await this.client.query(queryText, [semester, professor, evaluation, grade]);
+      rconsole.log(res.rows);
+   }
+}
